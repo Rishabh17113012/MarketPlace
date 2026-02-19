3997acd3628371c1327f2b04684a074b20f6803e
Rishabh Dev Mani Tripathi <rishabhtripathi736@gmail.com>
2026-02-19 17:36:14 +0530

    first commit

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -45,7 +45,7 @@ export function AuthProvider({ children }: { children: ReactNode }) {
     if (error) throw error;
 
     // Alert user after successful signup
-    alert("You are now authenticated. Please log in again with your password.");
+    alert("Authentication mail has been sent on your provided mail.Please check it and verify your identity and log in again with your password");
 
     // Redirect user to login page
     window.location.href = "/auth"; // Update this path if your login page is different
